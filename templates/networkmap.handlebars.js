bb62065819ddb40a230afd50d35bcf00638ecb62
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2014-03-28 13:38:19 +0100

    U commit

--- templates/networkmap.handlebars.js
+++ templates/networkmap.handlebars.js
@@ -10,7 +10,9 @@ function program1(depth0,data) {
   var buffer = "", stack1, stack2;
   buffer += "\n	<li id=\"conn"
     + escapeExpression(((stack1 = ((stack1 = data),stack1 == null || stack1 === false ? stack1 : stack1.index)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "\" class=\"connection\">\n		<span class=\"icon-screen type_icon\"></span>\n		<ul class=\"ip_info\">\n			<li class=\"ip\">"
+    + "\" class=\"connected_ip\" onclick=\"app.enable_ip_info('"
+    + escapeExpression(((stack1 = ((stack1 = data),stack1 == null || stack1 === false ? stack1 : stack1.key)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "')\">\n		<span class=\"moonicon-screen type_icon\"></span>\n		<ul class=\"ip_info\">\n			<li class=\"ip\">"
     + escapeExpression(((stack1 = ((stack1 = data),stack1 == null || stack1 === false ? stack1 : stack1.key)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "</li>\n			<li class=\"hostname\">";
   if (stack2 = helpers.hostname) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
@@ -24,7 +26,7 @@ function program1(depth0,data) {
   return buffer;
   }
 
-  buffer += "<ul id=\"connections\">\n	";
+  buffer += "<ul id=\"conn_ips\">\n	";
   stack1 = helpers.each.call(depth0, (depth0 && depth0.connections), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "\n</ul>";
