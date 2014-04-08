f9b010e1696f71134f35dd86931a0b3abdc6a0f9
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2014-04-08 12:40:46 +0200

    Add scroll bar to connection details.

--- templates/connectiondetails.handlebars.js
+++ templates/connectiondetails.handlebars.js
@@ -8,18 +8,18 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\n		<dt>"
+  buffer += "\n			<dt>"
     + escapeExpression(((stack1 = ((stack1 = data),stack1 == null || stack1 === false ? stack1 : stack1.key)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</dt>\n		<dd>"
+    + "</dt>\n			<dd>"
     + escapeExpression((typeof depth0 === functionType ? depth0.apply(depth0) : depth0))
-    + "</dd>\n	";
+    + "</dd>\n		";
   return buffer;
   }
 
-  buffer += "<hr/>\n<dl class=\"dl-horizontal\">\n	";
+  buffer += "<hr/>\n<div class=\"ip-info-box\">\n	<dl class=\"dl-horizontal\">\n		";
   stack1 = helpers.each.call(depth0, (depth0 && depth0.info), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n</dl>\n";
+  buffer += "\n	</dl>\n</div>\n";
   return buffer;
   });
 })();
